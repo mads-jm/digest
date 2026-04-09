c8ff17b35b0ce05e1304548c1cbcafce10213734
Joseph Madigan <mads.flac@proton.me>
2026-04-08 19:19:18 -0700

    legacy_src - cleanup post fork

--- frontend/src/assets/constants.js
+++
@@ -1,7 +0,0 @@
-export const color1E = "#1E1E1E";
-export const colorD9 = "#D9D9D9";
-export const colorE9 = "#E9E9E9";
-export const colorTP = "transparent";
-export const color00 = "#000000";
-export const colorB0 = "#B0B0B0";
-export const emailsPerPage = 15;
